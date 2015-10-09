5ac9e80aacca62a35c2385f4c0a157e460757499
Ranjib Dey <ranjib@linux.com>
2015-10-08 23:51:55 -0700

    use lscpu API

--- client/fingerprint/cpu.go
+++ client/fingerprint/cpu.go
@@ -21,51 +21,37 @@ func NewCPUFingerprint(logger *log.Logger) Fingerprint {
 }
 
 func (f *CPUFingerprint) Fingerprint(cfg *config.Config, node *structs.Node) (bool, error) {
-	cpuInfo, err := cpu.CPUInfo()
+	cpuInfo, err := cpu.GetCPUStat()
 	if err != nil {
 		f.logger.Println("[WARN] Error reading CPU information:", err)
 		return false, err
 	}
 
-	var numCores int32
 	var mhz float64
-	var modelName string
-
-	// Assume all CPUs found have same Model. Log if not.
-	// If CPUInfo() returns nil above, this loop is still safe
-	for _, c := range cpuInfo {
-		numCores += c.Cores
-		mhz += c.Mhz
-
-		if modelName != "" && modelName != c.ModelName {
-			f.logger.Println("[WARN] Found different model names in the same CPU information. Recording last found")
-		}
-		modelName = c.ModelName
+	if cpuInfo.CPUMHz > 0 {
+		mhz = cpuInfo.CPUMHz
+	} else if cpuInfo.CPUMaxMHz > 0 {
+		mhz = cpuInfo.CPUMaxMHz
+	} else {
 	}
-	// Get average CPU frequency
-	mhz /= float64(len(cpuInfo))
-
 	if mhz > 0 {
 		node.Attributes["cpu.frequency"] = fmt.Sprintf("%.6f", mhz)
 	}
 
-	if numCores > 0 {
-		node.Attributes["cpu.numcores"] = fmt.Sprintf("%d", numCores)
+	if cpuInfo.CPUs > 0 {
+		node.Attributes["cpu.numcores"] = fmt.Sprintf("%d", cpuInfo.CPUs)
 	}
 
-	if mhz > 0 && numCores > 0 {
-		tc := float64(numCores) * mhz
-		node.Attributes["cpu.totalcompute"] = fmt.Sprintf("%.6f", tc)
-
+	if mhz > 0 && cpuInfo.CPUs > 0 {
+		node.Attributes["cpu.totalcompute"] = fmt.Sprintf("%.6f", mhz)
 		if node.Resources == nil {
 			node.Resources = &structs.Resources{}
 		}
 
-		node.Resources.CPU = int(tc)
+		node.Resources.CPU = int(mhz)
 	}
-
-	if modelName != "" {
-		node.Attributes["cpu.modelname"] = modelName
+	if cpuInfo.ModelName != "" {
+		node.Attributes["cpu.modelname"] = cpuInfo.ModelName
 	}
 
 	return true, nil
